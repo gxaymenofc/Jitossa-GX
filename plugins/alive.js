b2f1a604298d62da14c5a7dd5df01b6be5a42a98
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-06-28 21:28:36 +0100

    🇲🇦 𝙷𝙰𝚁𝙳𝚈 - 𝙱𝙾𝚃 - 𝙼𝙳 👑

--- plugins/alive.js
+++ plugins/alive.js
@@ -5,9 +5,9 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "./src/ALIVE.mp3"
-    let url = "https://github.com/omarcharaf1"
-    let murl = "www.instagram.com/ovmar_1"
-    let img = "https://telegra.ph/file/e94d3e61da4c83e493c1e.png"
+    let url = "https://github.com/HardyBots"
+    let murl = "www.instagram.com/hg_hardy"
+    let img = "https://telegra.ph/file/bb617917b5b6c3454a217.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -22,9 +22,9 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
           mentionedJid: [m.sender],
           externalAdReply: {
           title: "البوت يعمل الأن",
-          body: "JITOSSA  BOT",
+          body: "HARDY  BOT",
           thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/HcXWPVjjVkY5JIw88Xokg5',
+          sourceUrl: 'https://chat.whatsapp.com/KeQRikyqnYFE1vm7UY85QZ',
           mediaType: 1,
           renderLargerThumbnail: true
           }}

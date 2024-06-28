86f0836500854efcdf4ac70c760f7331b7819250
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-06-28 21:22:04 +0100

    🇲🇦 𝙷𝙰𝚁𝙳𝚈 - 𝙱𝙾𝚃 - 𝙼𝙳 👑

--- plugins/_flaming10.js
+++ plugins/_flaming10.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args }) => {
   if (!args[0]) throw '*_أكتب نص مع هاذا الأمر لإنشاء لوغو_*\n\n *مثال الإستخدام*\n .logo-coffee JITOSSA'
   m.reply('*_جارى إنشاء لوغو لك قريبا ..._*')
   let res = `https://api.tiodevhost.my.id/api/photooxy/coffee-cup?text=${response[0]}`
-  conn.sendFile(m.chat, res, 'coffee-cu.jpg', `> _JITOSSA LOGO_ \n\n www.instagram.com/ovmar_1`, m, false)
+  conn.sendFile(m.chat, res, 'coffee-cu.jpg', `> _HARDY LOGO_ \n\n www.instagram.com/hg_hardy`, m, false)
 }
 handler.help = ['logo-coffee'].map(v => v + ' <text>')
 handler.tags = ['maker'']
